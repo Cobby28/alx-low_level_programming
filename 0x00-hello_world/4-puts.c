1d8a851b7c96e6b3d9a151a2be39136011930a01
Cobby28 <rockantwi28@gmail.com>
2022-09-08 09:29:46 -0700

    roll

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,2 @@
 #!/bin/bash
-#include <studio.h>
-
-/..
-.main - print the string in the put function
-.
-.Description: using the main function
-.this program prints "Programing ls like building a multilingual puzzel
-.Return: 0
-./
-int main(void)
-(
- 	puts("\"Programing ls like building a multilingual puzzle):
-)
+#include <stdio.h>
