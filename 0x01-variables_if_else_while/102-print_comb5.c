0ec740fcd021c7942a0bdd5b9205e3f4f9cdc600
Cobby28 <rockantwi28@gmail.com>
2022-09-09 06:51:32 -0700

    lot

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,79 +1,47 @@
-/*
- *
- * * File: 102-print_comb5.c
- *
- * * Auth: Brennan D Baraban
- *
- * */
-
- 
-
 #include <stdio.h>
 
- 
-
 /**
- *
- * * main - Prints all possible combinations of two two-digit numbers,
- *
- * *        ranging from 0-99, separated by a comma followed by a space.
- *
- * *
- *
- * * Return: Always 0.
- *
- * */
-
+  * main - Prints 3 combination of numbers
+  *
+  * Return: Always (Success)
+  */
 int main(void)
-
 {
+	int c, i, k, j;
 
-	                int num1, num2;
+	for (c = 48; c <= 57; c++)
+	{
+		for (i = 48; i <= 57; i++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				for (j = 48; j <= 57; j++)
+				{
+					if (((k + j) > (c + i) &&  k >= c) || c < k)
+					{
+						putchar(c);
+						putchar(i);
+						putchar(' ');
+						putchar(k);
+						putchar(j);
 
-			 
+					if (c + i + k + j == 227 && c == 57)
+					{
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+					}
+				}
+			}
+		}
+	}
 
-			                for (num1 = 0; num1 <= 98; num1++)
+	putchar('\n');
 
-						                {
-
-									                                for (num2 = num1 + 1; num2 <= 99; num2++)
-
-														                                {
-
-																			                                                putchar((num1 / 10) + '0');
-
-																									                                                putchar((num1 % 10) + '0');
-
-																															                                                putchar(' ');
-
-																																					                                                putchar((num2 / 10) + '0');
-
-																																											                                                putchar((num2 % 10) + '0');
-
-																																																	 
-
-																																																	                                                if (num1 == 98 && num2 == 99)
-
-																																																								                                                                continue;
-
-																																																							 
-
-																																																							                                                putchar(',');
-
-																																																													                                                putchar(' ');
-
-																																																																			                                }
-
-													                }
-
-					 
-
-					                putchar('\n');
-
-							 
-
-							                return (0);
-
+	return (0);
 }
-
 
