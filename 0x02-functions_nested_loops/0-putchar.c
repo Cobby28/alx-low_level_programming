13f22c86a8599f8b1bfa32f76dfe9f1b0345e8a9
Cobby28 <rockantwi28@gmail.com>
2022-09-13 05:24:53 -0700

    us

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,43 +1,22 @@
 #include "main.h"
 
-
-
 /**
- *
- *   * main - Entry point
- *
- *     *
- *
- *       * Return: Always 0
- *
- *         */
-
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
-
 {
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 
-		_putchar('H');
-
-			_putchar('o');
-
-				_putchar('l');
-
-					_putchar('b');
-
-						_putchar('e');
-
-							_putchar('r');
-
-								_putchar('t');
-
-									_putchar('o');
-
-										_putchar('n');
-
-											_putchar('\n');
-
-
-
-												return (0);
-
+	return (0);
 }
