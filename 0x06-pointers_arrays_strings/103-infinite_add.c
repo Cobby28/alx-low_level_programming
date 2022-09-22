0d4bc9f02ef2aaa444b1453acb0efc60b221500a
Cobby28 <rockantwi28@gmail.com>
2022-09-21 19:44:49 -0700

    trust

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -15,19 +15,19 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	rl = i = l1 = l2 = sum = num1 = num2 = carry = 0;
 	while (n1[l1] != '\0')
 		l1++;
-	while (n2[l2] != '\0')
-		l2++;
+	while (n2[l3] != '\0')
+		l3++;
 	if (l1 + 2 > size_r || l2 + 2 > size_r)
 		return (0);
 	l1--;
-	l2--;
+	l3--;
 	while (i <= l1 || i <= l2)
 	{
 		num1 = num2 = 0;
 		if (i <= l1)
 			num1 = n1[l1 - i] - '0';
-		if (i <= l2 && (l2 - i) >= 0)
-			num2 = n2[l2 - i] - '0';
+		if (i <= l3 && (l3 - i) >= 0)
+			num2 = n2[l3 - i] - '0';
 		sum = num1 + num2 + carry;
 		if (sum >= 10)
 		{
